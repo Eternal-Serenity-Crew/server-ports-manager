5208dd3041dbad0eb6a7263b27818861780efbc7
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-21 16:41:05 +0300

    feat(core): created crud interface with default repo getters

---
+++ src/main/kotlin/org/esc/serverportsmanager/services/interfaces/CrudService.kt
@@ -0,0 +1,20 @@
+package org.esc.serverportsmanager.services.interfaces
+
+import org.esc.serverportsmanager.services.dto.DtoClass
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CrudService<T, ID> {
+    val repository: JpaRepository<T, ID>
+
+    fun getAll(): List<T> = repository.findAll()
+    fun getAllByIds(ids: List<ID>): List<T> = repository.findAllById(ids)
+    fun getById(id: ID): T? = id?.let { repository.findById(it) }?.get()
+
+    fun <D: DtoClass> create(item: D)
+    fun <D: DtoClass> createAll(items: List<D>)
+
+    fun <D: DtoClass> update(item: D)
+
+    fun deleteById(id: ID) = id?.let { repository.deleteById(it) }
+    fun deleteAll() = repository.deleteAll()
+}
