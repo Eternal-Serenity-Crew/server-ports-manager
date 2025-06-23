2305e1456ff9773f4fed1ed705e032e63bc9168b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-23 23:10:27 +0300

    refactor(core): removed old classes

--- src/main/kotlin/org/esc/serverportsmanager/validators/CompareDtoClasses.kt
+++
@@ -1,8 +0,0 @@
-package org.esc.serverportsmanager.validators
-
-import org.esc.serverportsmanager.dto.DtoClass
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class CompareDtoClasses(val value: KClass<out DtoClass>)
