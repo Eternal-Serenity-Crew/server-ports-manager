5d325b96f396aff41f28bcbc9ef7ed620cf35b2d
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-23 21:14:17 +0300

    feat(api): created exceptions & exceptions handler

--- src/main/kotlin/org/esc/serverportsmanager/io/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/esc/serverportsmanager/io/handlers/GlobalExceptionHandler.kt
@@ -1,17 +1,17 @@
 package org.esc.serverportsmanager.io.handlers
 
 import org.esc.serverportsmanager.exceptions.JwtAuthenticationException
+import org.esc.serverportsmanager.exceptions.NotFoundException
 import org.esc.serverportsmanager.io.BasicErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.context.request.WebRequest
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(Exception::class)
-    fun handleAllExceptions(ex: Exception, request: WebRequest): ResponseEntity<BasicErrorResponse> {
+    fun handleAllExceptions(ex: Exception): ResponseEntity<BasicErrorResponse> {
         val errorResponse = BasicErrorResponse(
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             message = ex.message ?: "Unexpected error"
@@ -20,8 +20,18 @@ class GlobalExceptionHandler {
         return ResponseEntity(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(ex: NotFoundException): ResponseEntity<BasicErrorResponse> {
+        val errorResponse = BasicErrorResponse(
+            status = HttpStatus.NOT_FOUND.value(),
+            message = ex.message
+        )
+
+        return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
+    }
+
     @ExceptionHandler(JwtAuthenticationException::class)
-    fun handleWrongUserDataException(ex: JwtAuthenticationException, request: WebRequest): ResponseEntity<BasicErrorResponse> {
+    fun handleJwtAuthenticationException(ex: JwtAuthenticationException): ResponseEntity<BasicErrorResponse> {
         val errorResponse = ex.message?.let {
             BasicErrorResponse(
                 status = HttpStatus.UNAUTHORIZED.value(),
@@ -29,6 +39,6 @@ class GlobalExceptionHandler {
             )
         }
 
-        return ResponseEntity(errorResponse, HttpStatus.FORBIDDEN)
+        return ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED)
     }
 }
