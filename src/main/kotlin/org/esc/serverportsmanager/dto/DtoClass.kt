56a497f5ccf4461d1b735ce365690d2d56752bdb
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-24 23:52:45 +0300

    feat(core): added extension function to cast Dto to Response model

--- src/main/kotlin/org/esc/serverportsmanager/dto/DtoClass.kt
+++ src/main/kotlin/org/esc/serverportsmanager/dto/DtoClass.kt
@@ -1,3 +1,9 @@
 package org.esc.serverportsmanager.dto
 
+import org.esc.serverportsmanager.io.BasicSuccessfulResponse
+
 interface DtoClass
+
+fun <T : DtoClass> T.toHttpResponse(): BasicSuccessfulResponse<T> {
+    return BasicSuccessfulResponse(this)
+}
